f452bafc5d0577f24be8dd43f67a0eb0b6f77ae4
TecExpo <142141994+TecExpo@users.noreply.github.com>
2025-02-25 23:58:26 +0530

    Create federated-ai-threat-monitoring.py

---
+++ security/federated-ai-threat-monitoring.py
@@ -0,0 +1,8 @@
+security/
+│   │-- jwt-auth.py
+│   │-- zero-trust-auth.py
+│   │-- ai-threat-detection.py
+│   │-- blockchain-id-verification.py
+│   │-- post-quantum-encryption.py
+│   │-- federated-ai-threat-monitoring.py
+│   │-- homomorphic-encryption.py
