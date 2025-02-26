5606a34a6ea6b6e2145da1f4c0def24f701b37d5
TecExpo <142141994+TecExpo@users.noreply.github.com>
2025-02-26 08:26:50 +0530

    Create encryption.py

---
+++ security/homomorphic-encryption/encryption.py
@@ -0,0 +1,5 @@
+│── security/                           # AI-Powered Security  
+│   ├── homomorphic-encryption/  
+│   │   ├── encryption.py                # Secure AI Model Execution  
+│   ├── zero-trust/  
+│   │   ├── istio-rbac.yaml              # Istio RBAC
